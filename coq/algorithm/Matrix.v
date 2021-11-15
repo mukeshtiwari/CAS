b1c646f8e087921a5e46961b960c6ed2c931a05a
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2021-11-15 16:53:04 +1100

    finished the lists proof

--- coq/algorithm/Matrix.v
+++ coq/algorithm/Matrix.v
@@ -6,6 +6,102 @@ From CAS Require Import coq.common.compute
   coq.eqv.theory coq.sg.properties.
 Import ListNotations.
 
+Section Lfn.
+  (* This is temporary section and the functions defined 
+    here will move to another file.*)
+  Variables (A : Type)
+    (eqA : brel A)
+    (refA : brel_reflexive A eqA)
+    (symA : brel_symmetric A eqA)
+    (trnA : brel_transitive A eqA).
+
+  Fixpoint no_dup (l : list A) : bool :=
+    match l with
+    | [] => true
+    | h :: t => negb (in_list eqA t h) &&
+        no_dup t
+    end.
+
+  Fixpoint list_equality (l₁ l₂ : list A) : bool := 
+    match l₁ with 
+    | [] => match l₂ with 
+      | [] => true
+      | _ => false
+      end
+    | h₁ :: t₁ => match l₂ with
+      | [] => false 
+      | h₂ :: t₂ => (eqA h₁ h₂) && (list_equality t₁ t₂) 
+      end
+    end.   
+
+  Lemma list_equality_refl : forall l : list A, list_equality l l = true.
+  Proof.
+    induction l.
+    + simpl; reflexivity.
+    + simpl. apply Bool.andb_true_iff.
+      split. apply refA. apply IHl.
+  Qed.
+  
+  Lemma list_mem_not : forall (l : list A) (c a : A), eqA c a = true ->
+    in_list eqA l a = false -> in_list eqA l c = false.
+  Proof.
+  Admitted.
+
+  Lemma list_mem_true_false : forall (l : list A) (a c : A),
+    in_list eqA l a = false -> in_list eqA l c = true -> eqA c a = false.
+  Proof.
+  Admitted. 
+
+  Lemma list_split : forall (l : list A) (c : A),
+    l <> [] -> in_list eqA l c = true -> 
+    no_dup l = true -> exists l₁ l₂ : list A, 
+    list_equality l (l₁ ++ [c] ++ l₂) = true /\ 
+    in_list eqA l₁ c = false /\ 
+    in_list eqA l₂ c = false.
+  Proof.
+    induction l; simpl.
+    + intros ? H₁ H₂ H₃.
+      inversion H₂.
+    + intros c H₁ H₂ H₃.
+      apply Bool.andb_true_iff in H₃.
+      destruct H₃ as [Hl₃ Hr₃].
+      apply Bool.orb_true_iff in H₂.
+      destruct H₂ as [H₂ | H₂].
+      exists [], l; simpl; subst.
+      apply Bool.negb_true_iff in Hl₃.
+      split. apply Bool.andb_true_iff.
+      split. apply symA. apply H₂.
+      apply list_equality_refl.
+      split. auto.
+      apply list_mem_not with (a := a).
+      exact H₂. exact Hl₃.
+      (* l = [] \/ l <> []*)
+      destruct l as [|b l].
+      - inversion H₂.
+      - assert (Ht : b :: l <> []).
+        intro Hf. inversion Hf.
+        destruct (IHl c Ht H₂ Hr₃) as 
+         [la [lb [Ha [Hb Hc]]]].
+        exists (a :: la), lb.
+        assert (Hlf : (a :: la) ++ c :: lb = a :: la ++ c :: lb).
+        simpl. reflexivity.
+        rewrite Hlf. clear Hlf.
+        apply Bool.negb_true_iff in Hl₃.
+        split. apply Bool.andb_true_iff.
+        split. apply refA.
+        exact Ha.
+        split. simpl.
+        apply Bool.orb_false_iff.
+        split. apply list_mem_true_false with (l := b :: l).
+        exact Hl₃. exact H₂.
+        exact Hb. exact Hc.
+  Qed.
+        
+    
+
+Section Lfn.
+
+
 
 Section Matrix.
   Variables 
@@ -276,6 +372,7 @@ Section Matrix.
       fun (c d : Node) => sum_fn (fun y => (m₁ c y * m₂ y d)) l.
 
 
+
     Lemma push_mul_right_gen : forall a b c d fn gn, 
       fn =r= gn = true -> 
       (a * b + c) * d + fn =r= a * b * d + c * d + gn = true.
@@ -286,7 +383,8 @@ Section Matrix.
     Qed.
 
     (* This need right distributive (a + b) * c = a * c + b * c*)  
-    Lemma push_mul_right_sum_fn : forall (l₂ l₁ : list Node) (m₁ m₂ m₃ : Matrix) a x x0,
+    Lemma push_mul_right_sum_fn : forall (l₂ l₁ : list Node) 
+      (m₁ m₂ m₃ : Matrix) a x x0,
       sum_fn (λ y : Node,
         ((m₁ x a * m₂ a y + 
           sum_fn (λ y0 : Node, m₁ x y0 * m₂ y0 y) l₁) * m₃ y x0)) l₂ =r= 
@@ -303,9 +401,9 @@ Section Matrix.
       
 
     Local Lemma rewrite_gen_ind : forall a b c d e f g, 
-    a * d + f =r= g = true -> 
-    a * (b * c + d) + (e * c + f) =r= 
-    (a * b + e) * c + g = true.
+      a * d + f =r= g = true -> 
+      a * (b * c + d) + (e * c + f) =r= 
+      (a * b + e) * c + g = true.
     Proof.
       intros.
       assert (Ht : a * (b * c + d) + (e * c + f) =r= 
@@ -368,150 +466,189 @@ Section Matrix.
     Qed.
 
 
-  Lemma matrix_mul_gen_assoc : forall l₁ l₂ m₁ m₂ m₃ (c d : Node),
-    (matrix_mul_gen m₁ (matrix_mul_gen m₂ m₃ l₂) l₁ c d) =r= 
-    (matrix_mul_gen (matrix_mul_gen m₁ m₂ l₁) m₃ l₂ c d) = true.
-  Proof.
-    intros.
-      revert l₁ l₂ m₁ m₂ m₃ c d.
-    unfold matrix_mul_gen; induction l₁; simpl;
-    intros ? ? ? ? ? ?. 
-    +
-      induction l₂; simpl.
-      ++ apply refR.
-      ++ apply symR.
-        assert (Ht: 0 * m₃ a d + sum_fn (λ y : Node, 0 * m₃ y d) l₂ =r= 
-          0 + sum_fn (λ y : Node, 0 * m₃ y d) l₂ = true).
-        apply congrP. apply zero_left_anhilator_mul.
-        apply refR. rewrite <-Ht; clear Ht.
+    Lemma matrix_mul_gen_assoc : forall l₁ l₂ m₁ m₂ m₃ (c d : Node),
+      (matrix_mul_gen m₁ (matrix_mul_gen m₂ m₃ l₂) l₁ c d) =r= 
+      (matrix_mul_gen (matrix_mul_gen m₁ m₂ l₁) m₃ l₂ c d) = true.
+    Proof.
+      intros.
+        revert l₁ l₂ m₁ m₂ m₃ c d.
+      unfold matrix_mul_gen; induction l₁; simpl;
+      intros ? ? ? ? ? ?. 
+      +
+        induction l₂; simpl.
+        ++ apply refR.
+        ++ apply symR.
+          assert (Ht: 0 * m₃ a d + sum_fn (λ y : Node, 0 * m₃ y d) l₂ =r= 
+            0 + sum_fn (λ y : Node, 0 * m₃ y d) l₂ = true).
+          apply congrP. apply zero_left_anhilator_mul.
+          apply refR. rewrite <-Ht; clear Ht.
+          apply congrR. apply refR.
+          assert (Ht : 0 + sum_fn (λ y : Node, 0 * m₃ y d) l₂ =r=
+            sum_fn (λ y : Node, 0 * m₃ y d) l₂ = true).
+          apply zero_left_identity_plus. apply symR in Ht.
+          rewrite <-Ht. apply congrR.
+          exact IHl₂. apply refR.
+      (* inductive case *)
+      + specialize (IHl₁ l₂ m₁ m₂ m₃ c d).
+        (* This one is going to be tricky *)
+        assert (Ht: m₁ c a * sum_fn (λ y : Node, m₂ a y * m₃ y d) l₂ +
+          sum_fn 
+            (λ y : Node, m₁ c y * 
+              sum_fn (λ y0 : Node, m₂ y y0 * m₃ y0 d) l₂) l₁ =r=
+          m₁ c a * sum_fn (λ y : Node, m₂ a y * m₃ y d) l₂ + 
+          sum_fn
+            (λ y : Node,
+              sum_fn (λ y0 : Node, m₁ c y0 * m₂ y0 y) l₁ * m₃ y d) l₂ = true).
+        apply congrP.
+        apply refR. exact IHl₁.
+        rewrite <-Ht.
         apply congrR. apply refR.
-        assert (Ht : 0 + sum_fn (λ y : Node, 0 * m₃ y d) l₂ =r=
-          sum_fn (λ y : Node, 0 * m₃ y d) l₂ = true).
-        apply zero_left_identity_plus. apply symR in Ht.
-        rewrite <-Ht. apply congrR.
-        exact IHl₂. apply refR.
-    (* inductive case *)
-    + specialize (IHl₁ l₂ m₁ m₂ m₃ c d).
-      (* This one is going to be tricky *)
-      assert (Ht: m₁ c a * sum_fn (λ y : Node, m₂ a y * m₃ y d) l₂ +
-        sum_fn 
-          (λ y : Node, m₁ c y * 
-            sum_fn (λ y0 : Node, m₂ y y0 * m₃ y0 d) l₂) l₁ =r=
-        m₁ c a * sum_fn (λ y : Node, m₂ a y * m₃ y d) l₂ + 
-        sum_fn
-          (λ y : Node,
-             sum_fn (λ y0 : Node, m₁ c y0 * m₂ y0 y) l₁ * m₃ y d) l₂ = true).
-      apply congrP.
-      apply refR. exact IHl₁.
-      rewrite <-Ht.
-      apply congrR. apply refR.
-      clear Ht; clear IHl₁.
-      apply symR.
-      induction l₂; simpl.
-      ++ assert (Ht : m₁ c a * 0 + 0 =r= 0 + 0 = true).
-        apply congrP. apply zero_right_anhilator_mul.
-        apply refR.
-        rewrite <-Ht. apply congrR.
-        apply refR. apply symR.
-        apply zero_left_identity_plus.
-      ++ apply rewrite_gen_ind. exact IHl₂.
-  Qed.
+        clear Ht; clear IHl₁.
+        apply symR.
+        induction l₂; simpl.
+        ++ assert (Ht : m₁ c a * 0 + 0 =r= 0 + 0 = true).
+          apply congrP. apply zero_right_anhilator_mul.
+          apply refR.
+          rewrite <-Ht. apply congrR.
+          apply refR. apply symR.
+          apply zero_left_identity_plus.
+        ++ apply rewrite_gen_ind. exact IHl₂.
+    Qed.
 
-
-
-  Definition matrix_mul (m₁ m₂ : Matrix) := 
-    matrix_mul_gen m₁ m₂ finN.
-
-  
-  Lemma matrix_mul_assoc : forall m₁ m₂ m₃ (c d : Node),
-    matrix_mul m₁ (matrix_mul m₂ m₃) c d =r= 
-    matrix_mul (matrix_mul m₁ m₂) m₃ c d = true.
-  Proof.
-    unfold matrix_mul.
-    apply matrix_mul_gen_assoc.
-  Qed.
-
-
-
-  (* Now I need Matrix exponentiation *)
-  (* write a slow one, nat, and fast one, Binary nat and 
-    prove that they are equal. 
-    For nat, 
-    proofs will be easy, but slow computation. For Binary nat, 
-    proofs be difficult but computation will be fast. 
-
-    Then show that 0-stable, then it reaches a fixpoint
-    *)
-  Fixpoint matrix_exp_unary (m : Matrix) (n : nat) : Matrix :=
-    match n with 
-    | 0%nat => I 
-    | S n' => matrix_mul m (matrix_exp_unary m n')
-    end.
-  
     
-  Fixpoint repeat_op_ntimes_rec (e : Matrix) (n : positive) : Matrix :=
-    match n with
-    | xH => e
-    | xO p => let ret := repeat_op_ntimes_rec e p in matrix_mul ret ret
-    | xI p => let ret := repeat_op_ntimes_rec e p in matrix_mul e (matrix_mul ret ret)
-    end.
 
-  Definition matrix_exp_binary (e : Matrix) (n : N) :=
-    match n with
-    | N0 => I
-    | Npos p => repeat_op_ntimes_rec e p 
-    end.
+
+    (* for this proof, I need l to be finite, non-empty, 
+      but more importantly, non-duplicate. 
+    *)
+    Lemma matrix_mul_left_identity : forall (l : list Node),
+      l <> [] -> (∀ x : Node, in_list eqN l x = true) -> NoDup l ->
+      forall (m : Matrix) (c d : Node),
+      matrix_mul_gen I m l c d =r= m c d = true.
+    Proof.
+      unfold matrix_mul_gen, I.
+      intros.
+      assert (Ht : exists l₁ l₂ : list Node, 
+        l = l₁ ++ [c] ++ l₂ /\ 
+        in_list eqN l₁ c = false /\ 
+        in_list eqN l₂ c = false). admit.
+      destruct Ht as [l₁ [l₂ [Ht₁ [Ht₂ Ht₃]]]].
+      rewrite Ht₁.
+
+      rewrite Ht.
+      (* Now, if c occurs just once in the list l, 
+        then (λ y : Node, (if c =n= y then 1 else 0) * m y d) 
+        is going to be (m c d) *) 
+      induction l; simpl; intros H₁ H₂ m c d.
+      + pose proof (H₂ c) as Hf.
+        inversion Hf.
+      + destruct l; simpl.
+        (* Now l is not empty *)
+        ++ simpl in H₂.
+          specialize (H₂ c).
+          apply Bool.orb_true_iff in H₂.
+          destruct H₂ as [H₂ | H₂].
+          rewrite H₂. admit.
+          inversion H₂.
+        ++ (* Now, here I need NoDup assumption. 
+          c =n= a = true, then first one is goind to be true and 
+          rest is 0 otherwise inductive hypothesis*) 
+
+
+
+    Definition matrix_mul (m₁ m₂ : Matrix) := 
+      matrix_mul_gen m₁ m₂ finN.
+
+    
+    Lemma matrix_mul_assoc : forall m₁ m₂ m₃ (c d : Node),
+      matrix_mul m₁ (matrix_mul m₂ m₃) c d =r= 
+      matrix_mul (matrix_mul m₁ m₂) m₃ c d = true.
+    Proof.
+      unfold matrix_mul.
+      apply matrix_mul_gen_assoc.
+    Qed.
+
+
+
+    (* Now I need Matrix exponentiation *)
+    (* write a slow one, nat, and fast one, Binary nat and 
+      prove that they are equal. 
+      For nat, 
+      proofs will be easy, but slow computation. For Binary nat, 
+      proofs be difficult but computation will be fast. 
+
+      Then show that 0-stable, then it reaches a fixpoint
+      *)
+    Fixpoint matrix_exp_unary (m : Matrix) (n : nat) : Matrix :=
+      match n with 
+      | 0%nat => I 
+      | S n' => matrix_mul m (matrix_exp_unary m n')
+      end.
+    
+      
+    Fixpoint repeat_op_ntimes_rec (e : Matrix) (n : positive) : Matrix :=
+      match n with
+      | xH => e
+      | xO p => let ret := repeat_op_ntimes_rec e p in matrix_mul ret ret
+      | xI p => let ret := repeat_op_ntimes_rec e p in matrix_mul e (matrix_mul ret ret)
+      end.
+
+    Definition matrix_exp_binary (e : Matrix) (n : N) :=
+      match n with
+      | N0 => I
+      | Npos p => repeat_op_ntimes_rec e p 
+      end.
+
+    
+    (* now prove that slow and fast computes the same value. *)
+
+    Lemma binnat_zero : forall (n : nat), 0%N = N.of_nat n -> n = 0%nat.
+    Proof.
+      induction n; try lia.
+    Qed.
 
   
-  (* now prove that slow and fast computes the same value. *)
-
-  Lemma binnat_zero : forall (n : nat), 0%N = N.of_nat n -> n = 0%nat.
-  Proof.
-    induction n; try lia.
-  Qed.
-
- 
-  Lemma binnat_odd : forall (p : positive) (n : nat), N.pos (xI p) = N.of_nat n -> 
-    exists k,  n = (2 * k + 1)%nat /\  (N.pos p) = (N.of_nat k).
-  Proof.
-    intros p n Hp.
-    destruct (Even.even_or_odd n) as [H | H].
-    apply Even.even_equiv in H. destruct H as [k Hk].
-    (* Even (impossible) Case *)
-    rewrite Hk in Hp; lia.
-    (* Odd (possible) case *)
-    apply Even.odd_equiv in H. destruct H as [k Hk].
-    rewrite Hk in Hp. exists k.
-    split. exact Hk. lia.
-  Qed.
+    Lemma binnat_odd : forall (p : positive) (n : nat), N.pos (xI p) = N.of_nat n -> 
+      exists k,  n = (2 * k + 1)%nat /\  (N.pos p) = (N.of_nat k).
+    Proof.
+      intros p n Hp.
+      destruct (Even.even_or_odd n) as [H | H].
+      apply Even.even_equiv in H. destruct H as [k Hk].
+      (* Even (impossible) Case *)
+      rewrite Hk in Hp; lia.
+      (* Odd (possible) case *)
+      apply Even.odd_equiv in H. destruct H as [k Hk].
+      rewrite Hk in Hp. exists k.
+      split. exact Hk. lia.
+    Qed.
 
 
 
-  Lemma binnat_even : forall (p : positive) (n : nat), N.pos (xO p) = N.of_nat n :> N -> 
-    exists k, n = (Nat.mul 2 k) /\  (N.pos p) = (N.of_nat k).
-  Proof.
-    intros p n Hp.
-    destruct (Even.even_or_odd n) as [H | H].
-    apply Even.even_equiv in H. destruct H as [k Hk].
-    (* Even (possible) case*)
-    rewrite Hk in Hp. exists k.
-    split. exact Hk. lia.
-    (* Odd (impossible) case *)
-    apply Even.odd_equiv in H. 
-    destruct H as [k Hk].
-    rewrite Hk in Hp. lia.
-  Qed. 
+    Lemma binnat_even : forall (p : positive) (n : nat), N.pos (xO p) = N.of_nat n :> N -> 
+      exists k, n = (Nat.mul 2 k) /\  (N.pos p) = (N.of_nat k).
+    Proof.
+      intros p n Hp.
+      destruct (Even.even_or_odd n) as [H | H].
+      apply Even.even_equiv in H. destruct H as [k Hk].
+      (* Even (possible) case*)
+      rewrite Hk in Hp. exists k.
+      split. exact Hk. lia.
+      (* Odd (impossible) case *)
+      apply Even.odd_equiv in H. 
+      destruct H as [k Hk].
+      rewrite Hk in Hp. lia.
+    Qed. 
 
-  (* 
-  Lemma push_out_e_unary_nat_gen : forall k1 k2 e c d , matrix_exp e (k1 + k2)  c d = 
-    matrix_mul (matrix_exp e k1)  (matrix_exp e k2) c d.
-  Proof.
-    induction k1.
-    + intros ? ?; simpl. 
-      (* requires I * m = m *)
-      admit.
-    + admit.
-  Admitted. *)
+    (* 
+    Lemma push_out_e_unary_nat_gen : forall k1 k2 e c d , matrix_exp e (k1 + k2)  c d = 
+      matrix_mul (matrix_exp e k1)  (matrix_exp e k2) c d.
+    Proof.
+      induction k1.
+      + intros ? ?; simpl. 
+        (* requires I * m = m *)
+        admit.
+      + admit.
+    Admitted. *)
 
 
 
