03e60d770006439cc86f777fce02593418a7e33b
Mukesh Tiwari <mt883@cam.ac.uk>
2022-05-02 15:04:22 +0100

    Removed compute because we don't need it

--- coq/tr/left/plus_one.v
+++ coq/tr/left/plus_one.v
@@ -152,15 +152,12 @@ Section Verify.
 
 Theorem correct_ltr_plus_one :
     left_transform_plus_one = A2C_left_transform nat nat A_left_transform_plus_one. 
-Proof. compute. reflexivity. Qed. 
+Proof. reflexivity. Qed. 
 
 
 Theorem correct_mcas_ltr_plus_one : 
     mcas_ltr_plus_one = A2C_mcas_ltr _  _ A_mcas_ltr_plus_one.
-Proof. 
-    compute.
-    reflexivity.
-Qed.
+Proof. reflexivity. Qed.
 
 
 End Verify.   
