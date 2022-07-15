d98f6e48795cd680300c61f9d30ca1a26c132920
Mukesh Tiwari <mt883@cam.ac.uk>
2022-07-15 16:05:04 +0100

    comment

--- coq/bs/add_one.v
+++ coq/bs/add_one.v
@@ -78,6 +78,8 @@ Proof. intros idem lla lm [c1 | s1] [c2 | s2] [c3 | s3]; compute; intro A; auto.
 Qed. 
 
 
+(* anti absorption *)
+
 Lemma bops_add_one_not_left_anti_absorptive :
   bops_not_left_anti_absorptive (with_constant S) 
     (brel_sum brel_constant r) (c [+ann] b1) (c [+id] b2).
